a21014e14e3c9fcc03ac24767499557bca9e2104
Inma Gijón <igijoninf@gmail.com>
2022-09-18 22:18:24 +0200

    Webpack funcionando con módulos y exportaciones

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,5 +1,5 @@
 
-const saludar = ( nombre ) => {
+export const saludar = ( nombre ) => {
     console.log('Creando etiqueta h1');
     const h1 = document.createElement('h1');
     h1.innerText = `Hola, ${nombre}`;
