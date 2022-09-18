98b975ef86204e132aa89153572e1bb6b441c8fe
Inma Gijón <igijoninf@gmail.com>
2022-09-18 22:13:43 +0200

    Instalación de webpack y configuración inicial en package.json

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
+import { saludar } from './js/componentes';
 
+const nombre = 'Inma';
 
-const saludar = ( nombre ) => {
-    console.log('Creando etiqueta h1');
-    const h1 = document.createElement('h1');
-    h1.innerText = `Hola, ${nombre}`;
-    document.body.append(h1);
-} 
-
-saludar('Inma');
+saludar( nombre );
