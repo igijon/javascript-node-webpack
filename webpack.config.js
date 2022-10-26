18293ef97a6c7b7a3f41d0e24cd54dc3fdf3f06e
Inma Gijón <igijoninf@gmail.com>
2022-10-26 08:45:06 +0200

    Webpack sass configuration

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,8 @@ module.exports = {
                 use: ['style-loader', 'css-loader']
             },
             {
-                test: /styles.css$/,
-                use: [ MiniCssExtractPlugin.loader, 'css-loader']
+                test: /\.(c|sc|sa)ss$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             },
             {
                 test: /\.(png|jpe?g|gif)$/,
